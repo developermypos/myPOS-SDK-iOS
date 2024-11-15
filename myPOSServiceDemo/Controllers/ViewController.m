4018c0eabfac46f5cba0c1877f090e20baabcb6d
Ivaylo Zhelev <ivaylo.zhelev@mypos.com>
2024-11-15 17:52:36 +0200

    wifi availability

--- myPOSServiceDemo/Controllers/ViewController.m
+++ myPOSServiceDemo/Controllers/ViewController.m
@@ -50,6 +50,22 @@ static NSString * const kInitializedSegue = @"initialized";
     [self initializeSDKForDeviceMode:MPPOSDeviceModeUnknown];
 }
 
+- (IBAction)wifi:(id)sender {
+    UIAlertController *alertController = [UIAlertController alertControllerWithTitle:@"Connect through WIFI" message:@"POS device host and port:" preferredStyle:UIAlertControllerStyleAlert];
+    [alertController addTextFieldWithConfigurationHandler:^(UITextField * _Nonnull textField) {
+        textField.placeholder = @"host:port";
+    }];
+    UIAlertAction *confirmAction = [UIAlertAction actionWithTitle:@"OK" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+        [self initializeSDKWithURL:[[alertController textFields][0] text]];
+    }];
+    [alertController addAction:confirmAction];
+    UIAlertAction *cancelAction = [UIAlertAction actionWithTitle:@"Cancel" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        NSLog(@"Canelled");
+    }];
+    [alertController addAction:cancelAction];
+    [self presentViewController:alertController animated:YES completion:nil];
+}
+
 #pragma mark - Private Methods
 
 - (void)initializeSDKForDeviceMode:(MPPOSDeviceMode)mode {
@@ -62,5 +78,17 @@ static NSString * const kInitializedSegue = @"initialized";
                                                : [self performSegueWithIdentifier:kInitializedSegue sender:self];
                                      }];
 }
+
+- (void)initializeSDKWithURL:(NSString *)url {
+    [myPOSService startInitializationFromController:self
+                                             forURL:url
+                                     withCompletion:^(MPPOSDeviceMode posDeviceMode, NSError * _Nullable error) {
+                                         _initialized = error == nil;
+        
+                                         error ? [UIAlertController showAlertWithTitle:@"Error" message:error.localizedDescription fromController:self]
+                                               : [self performSegueWithIdentifier:kInitializedSegue sender:self];
+                                     }];
+}
+
 
 @end

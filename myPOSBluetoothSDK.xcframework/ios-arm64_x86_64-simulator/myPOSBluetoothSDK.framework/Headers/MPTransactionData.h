8ace396767eb713ab745e8f7f7a423a5aa216346
Ivaylo Zhelev <ivaylo.zhelev@mypos.com>
2025-02-18 18:01:28 +0200

    Extra requirements + fixes

--- myPOSBluetoothSDK.xcframework/ios-arm64_x86_64-simulator/myPOSBluetoothSDK.framework/Headers/MPTransactionData.h
+++ myPOSBluetoothSDK.xcframework/ios-arm64_x86_64-simulator/myPOSBluetoothSDK.framework/Headers/MPTransactionData.h
@@ -24,6 +24,34 @@ NS_ASSUME_NONNULL_BEGIN
 @property (nullable, nonatomic, strong) NSString *stan;
 @property (nullable, nonatomic, strong) NSString *rrn;
 @property (nullable, nonatomic, strong) NSString *reference;
+@property (nullable, nonatomic, strong) NSString *approval;
+@property (nullable, nonatomic, strong) NSString *embossName;
+@property (nullable, nonatomic, strong) NSString *signatureNotRequired;
+@property (nullable, nonatomic, strong) NSString *lastTransactionType;
+@property (nullable, nonatomic, strong) NSString *merchantAl1;
+@property (nullable, nonatomic, strong) NSString *merchantAl2;
+@property (nullable, nonatomic, strong) NSString *authCode;
+@property (nullable, nonatomic, strong) NSString *applicationPrefName;
+@property (nullable, nonatomic, strong) NSString *cvm;
+@property (nullable, nonatomic, strong) NSString *cardEntryMode;
+@property (nullable, nonatomic, strong) NSString *balanceAmount;
+@property (nullable, nonatomic, strong) NSString *balanceCurrency;
+@property (nullable, nonatomic, strong) NSString *tipAmount;
+@property (nullable, nonatomic, strong) NSString *declinedReason1;
+@property (nullable, nonatomic, strong) NSString *declinedReason2;
+@property (nullable, nonatomic, strong) NSString *expireDate;
+@property (nullable, nonatomic, strong) NSString *оperatorCode;
+@property (nullable, nonatomic, strong) NSString *logoType;
+@property (nullable, nonatomic, strong) NSString *logoGuid;
+@property (nullable, nonatomic, strong) NSString *customReceiptRow1;
+@property (nullable, nonatomic, strong) NSString *customReceiptRow2;
+@property (nullable, nonatomic, strong) NSString *terminalName;
+@property (nullable, nonatomic, strong) NSString *configLogo;
+@property (nullable, nonatomic, strong) NSString *dccCurrencyCode;
+@property (nullable, nonatomic, strong) NSString *dccAmount;
+@property (nullable, nonatomic, strong) NSString *dccCardExchangeRate;
+@property (nullable, nonatomic, strong) NSString *referenceNumber;
+@property (nullable, nonatomic, strong) NSString *referenceType;
 
 @end
 
